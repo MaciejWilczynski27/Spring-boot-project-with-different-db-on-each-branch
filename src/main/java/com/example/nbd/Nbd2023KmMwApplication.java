492dcf563faa97d1eb1a0fc399fe826663698479
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2023-10-13 09:26:43 +0200

    Starter project

---
+++ src/main/java/com/example/nbd/Nbd2023KmMwApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbd2023KmMwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbd2023KmMwApplication.class, args);
+    }
+
+}
